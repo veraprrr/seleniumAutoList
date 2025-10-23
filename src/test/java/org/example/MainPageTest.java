90b2e7719ce9d63e19d2a22bfd1f56469f4aad84
veraprrr <veravera404@yandex.ru>
2025-10-23 17:18:09 +0500

    lists

---
+++ src/test/java/org/example/MainPageTest.java
@@ -0,0 +1,63 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainPageTest {
+    private WebDriver driver;
+
+    @BeforeEach
+    void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.bing.com");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void search() {
+        String input = "Selenium.dev";
+        String searchUrl = "https://www.selenium.dev/";
+        WebElement searchField = driver.findElement(By.cssSelector("#sb_form_q"));
+        searchField.sendKeys(input);
+        searchField.submit();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
+        wait.until(ExpectedConditions.and(
+                ExpectedConditions.attributeContains(By.cssSelector("h2 > a[href]"), "href", "selenium"),
+                ExpectedConditions.elementToBeClickable(By.cssSelector("h2 > a[href]"))
+        ));
+        List<WebElement> results = driver.findElements(By.cssSelector("h2 > a[href]"));
+
+        clickElement(results, 0);
+        assertEquals(searchUrl, driver.getCurrentUrl(), "Не открылась первая вкладка");
+
+    }
+
+    public void clickElement(List<WebElement> results, int num) {
+        results.get(num).click();
+        int searchNumber = num + 1;
+        System.out.println("Кликнули по " + searchNumber + " элементу");
+    }
+}
